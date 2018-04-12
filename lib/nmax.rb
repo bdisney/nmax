0ba52f70f130521b0366b2836d121eb441407927
disne_y <bold.disney@gmail.com>
2018-04-12 12:07:20 +0300

    Add parse and display results logic. Also add spec for it.

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -1,5 +1,21 @@
-require "nmax/version"
-
 module Nmax
-  # Your code goes here...
+  autoload :VERSION, 'nmax/version'
+
+  DIGIT_FORMAT = /\d+/i
+
+  def self.parse(limit, stdin = $stdin.read)
+    results = []
+
+    stdin.each_line do |line|
+      line.scan(DIGIT_FORMAT) { |digit| results << digit.to_i }
+    end
+
+    results = results.uniq.max(limit)
+  end
+
+  private
+
+  def self.display_results(results)
+    results.each { |num| puts num }
+  end
 end
--- spec/nmax_spec.rb
+++ spec/nmax_spec.rb
@@ -1,11 +1,27 @@
 require "spec_helper"
 
 describe Nmax do
-  it "has a version number" do
-    expect(Nmax::VERSION).not_to be nil
+  let(:test_string) { StringIO.new('111assfgds222gsadfsda333') }
+  let(:results)     { Nmax.parse(2, test_string) }
+
+  it "returns integer values from parsed string" do
+    expect(Nmax.parse(3, test_string)).to eq([333, 222, 111])
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'returns numbers according to the limit' do
+    expect(Nmax.parse(2, test_string)).to eq([333, 222])
+  end
+
+  it 'returns nothing than limit is zero' do
+    expect(Nmax.parse(0, test_string)).to eq([])
+  end
+
+  it 'parsed integer values from file' do
+    expect(Nmax.parse(4, File.open('./spec/files/sample_text.txt'))).to eq([444555, 222333, 556, 111])
+  end
+
+  it 'display results to stdout' do
+    expect { Nmax.display_results(results) }.to output.to_stdout
+    expect { Nmax.display_results(results) }.to output("333\n222\n").to_stdout
   end
 end
